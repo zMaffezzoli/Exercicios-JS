d810923fb4d158f5e0a787cdcc60ba538820a8ce
zMaffezzoli <brunomaffezzolirodrigues0@gmail.com>
2024-01-15 14:11:58 -0300

    Years isnot months

--- Relogio/script.js
+++ Relogio/script.js
@@ -3,9 +3,9 @@ function Calendar(){
     let hour = info.substring(16,24)
     let date = info.substring(4,15).replace(/ /g, "/")
 
-    let years = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
  
-    date = date.substring(4,7) + (years.indexOf(date.substring(0,3)) + 1) + date.substring(6);
+    date = date.substring(4,7) + (months.indexOf(date.substring(0,3)) + 1) + date.substring(6);
 
     return {"hour": hour, "date": date};
 };
